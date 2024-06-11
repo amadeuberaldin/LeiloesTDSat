c34d89912f255b42313f857923a1ac95afdc8a23
Amadeu Beraldin <amadeu_beraldin@hotmail.com>
2024-06-11 12:22:59 -0300

    implementado o metodo para listar somente produtos vendidos

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -194,4 +194,46 @@ public class ProdutosDAO {
 
         return null;
     }
+
+    public ArrayList<ProdutosDTO> listarProdutosVendidos() {
+        String sql = "SELECT * FROM produtos WHERE status = 'Vendido'";
+        ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+        Connection conn = null;
+        PreparedStatement prep = null;
+        ResultSet resultset = null;
+
+        try {
+            conn = ConectaDAO.connectDB();
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+
+                listagem.add(produto);
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar os produtos vendidos: " + e.getMessage());
+        } finally {
+            try {
+                if (resultset != null) {
+                    resultset.close();
+                }
+                if (prep != null) {
+                    prep.close();
+                }
+                if (conn != null) {
+                    conn.close(); // Fechando a conexão após a operação
+                }
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, "Erro ao fechar a conexão: " + e.getMessage());
+            }
+        }
+
+        return listagem;
+    }
 }
